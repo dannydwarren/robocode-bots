3c9c75fbde95ce13875191e22e627560d9707e1f
Danny Warren <dannydwarren@gmail.com>
2021-03-25 10:34:46 -0600

    Init commit

---
+++ Sabor.X/GiantDeathRobot.cs
@@ -0,0 +1,25 @@
+﻿using Robocode;
+
+namespace Sabor.X
+{
+    public class GiantDeathRobot : Robot 
+    {
+        public override void Run()
+        {
+            TurnLeft(Heading - 90);
+            TurnGunRight(90);
+
+            while (true)
+            {
+                Ahead(5000);
+
+                TurnRight(90);
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(1);
+        }
+    }
+}
